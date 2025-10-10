f111221166b0f52c4525d2d0a008695a5437681c
Matheus Aroxa <matheusaroxa13@gmail.com>
2025-10-10 13:56:56 -0300

    Initial commit: Estrutura inicial do componente de cadastro

---
+++ crud-angular-material/src/app/cadastro/cadastro.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIcon } from '@angular/material/icon';
+import { MatButton } from '@angular/material/button';
+
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [
+    FlexLayoutModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatIcon,
+    MatButton
+  ],
+  templateUrl: './cadastro.html',
+  styleUrl: './cadastro.scss'
+})
+export class Cadastro {
+
+}
